55ad547eec585f4b78bbf2d98fd5848d87e6f95a
Raiyan <verdant.rs@gmail.com>
2023-02-25 02:08:23 -0500

    Multiple clients can connect to server

--- src/server.c
+++ src/server.c
@@ -3,10 +3,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <unistd.h>
 #define PORT 8080
+#define MAX_CLIENTS 5
+
+int newClientConnection(int (*client_socket_list)[MAX_CLIENTS], int sockfd) {
+  int client = accept(sockfd, NULL, NULL);
+
+  if (client < 0) {
+    perror("Client Connection Failed");
+  } else {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+      if (*client_socket_list[i] == 0) {
+        *client_socket_list[i] = client;
+        perror("Client Succeed");
+        return client;
+      }
+    }
+  }
+  return 0;
+}
 
 int main() {
+  fd_set current_sockets;
+  int client_socket_list[MAX_CLIENTS] = {
+      0,
+  };
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
     perror("Socket Creation Failed");
@@ -20,16 +43,37 @@ int main() {
   server_address.sin_port = htons(PORT);
   server_address.sin_addr.s_addr = INADDR_ANY;
 
-  bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+  int bind_status =
+      bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
 
-  listen(sockfd, 5);
+  if (bind_status < 0) {
+    perror("Unable to Bind Socket");
+    exit(EXIT_FAILURE);
+  }
+  int listen_status = listen(sockfd, MAX_CLIENTS);
 
-  int client;
+  if (listen_status < 0) {
+    perror("Unable to Listen");
+    exit(EXIT_FAILURE);
+  }
 
-  client = accept(sockfd, NULL, NULL);
-
-  char server_message[256] = "Hoho";
-  send(client, server_message, sizeof(server_message), 0);
-
+  while (1) {
+    FD_ZERO(&current_sockets);
+    FD_SET(sockfd, &current_sockets);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+      if (client_socket_list[i] > 0) {
+        FD_SET(client_socket_list[i], &current_sockets);
+      }
+    }
+    int select_status = select(MAX_CLIENTS, &current_sockets, NULL, NULL, NULL);
+    if (select_status < 0) {
+      perror("Error With Selecting");
+    } else if (select_status != 0) {
+      if (FD_ISSET(sockfd, &current_sockets)) {
+        newClientConnection(&client_socket_list, sockfd);
+      } else {
+      }
+    }
+  }
   return 0;
 }
