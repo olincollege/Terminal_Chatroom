36878a2459459d3cb3e54a5e19a28cc939d50d4f
Raiyan <verdant.rs@gmail.com>
2023-03-20 23:09:22 -0400

    Fixed error in client header

--- src/client.c
+++ src/client.c
@@ -1,5 +1,5 @@
-#include <arpa/inet.h>
 #include "chat_utilities.c"
+#include <arpa/inet.h>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -29,14 +29,14 @@ int check_connection_status(int connection_status) {
   return 0;
 }
 
-void read_buffer(char* buffer) {
+void read_buffer(char *buffer) {
   printf("\033[A");
   if (strlen(buffer) > 0 && buffer[strlen(buffer) - 1] == '\n') {
     buffer[strlen(buffer) - 1] = '\0';
   }
 }
 
-void write_to_server(int sockfd, char* packet, fd_set read_fd) {
+void write_to_server(int sockfd, char *packet, fd_set read_fd) {
   if (write(sockfd, packet, strlen(packet)) == -1) {
     printf("Write Failed\n");
   } else {
@@ -44,7 +44,7 @@ void write_to_server(int sockfd, char* packet, fd_set read_fd) {
   }
 }
 
-int read_from_server(int packet_received, char* buffer) {
+int read_from_server(int packet_received, char *buffer) {
   if (packet_received == -1) {
     printf("Receive Failed\n");
     return 0;
@@ -57,66 +57,3 @@ int read_from_server(int packet_received, char* buffer) {
     return 0;
   }
 }
-
-
-// int main() {
-//   // int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-//   int sockfd = -1;
-//   if (check_socket_creation(sockfd)) {
-//     return 0;
-//   };
-
-//   struct sockaddr_in server_address;
-//   server_address.sin_family = AF_INET;
-//   server_address.sin_port = htons(PORT);
-//   server_address.sin_addr.s_addr = INADDR_ANY;
-
-//   int connection_status = connect(sockfd, (struct sockaddr *)&server_address,
-//                                   sizeof(server_address));
-
-//   check_connection_status(connection_status);
-
-//   char username[USERNAME_BUFFER_SIZE + 1];
-//   get_username(username);
-
-//   char buffer[1024];
-//   char packet[1024];
-//   fd_set read_fd, write_fd;
-//   FD_ZERO(&read_fd);
-//   FD_ZERO(&write_fd);
-//   FD_SET(sockfd, &read_fd);
-//   FD_SET(sockfd, &write_fd);
-//   struct timeval tv;
-
-//   while (1) {
-//     tv.tv_sec = 0;
-//     tv.tv_usec = 100000;
-//     fd_set temp_read = read_fd;
-//     fd_set temp_write = write_fd;
-//     int select_status = select(sockfd + 1, &temp_read, &temp_write, NULL, &tv);
-//     if (select_status < 0) {
-//       perror("Error With Selecting");
-//     }
-
-//     if (FD_ISSET(sockfd, &temp_read)) {
-//       int packet_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
-//       if (read_from_server(packet_received, buffer)) {
-//         break;
-//       };
-//       FD_SET(sockfd, &read_fd);
-//     }
-
-//     if (FD_ISSET(STDIN_FILENO, &temp_read)) {
-//       fgets(buffer, sizeof(buffer), stdin);
-//       read_buffer(buffer);
-//       create_packet(packet, username, buffer);
-//       write_to_server(sockfd, packet, read_fd);
-//     }
-
-//     FD_SET(sockfd, &read_fd);
-//     FD_SET(STDIN_FILENO, &read_fd);
-//   }
-
-//   // close(sockfd);
-//   return 0;
-// }
--- src/client.h
+++ src/client.h
@@ -1,20 +1,22 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include "chat_utilities.c"
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 /**
  * Checks if the socket was created successfully, throws an error if not.
  *
  * @param sockfd An integer representing the socket the client is connected to.
- * @return 
+ * @return
  */
 int check_socket_creation(int sockfd);
 
@@ -29,42 +31,44 @@ int check_connection_status(int connection_status);
 /**
  * Reads the stdin buffer and clears the line the user types after enter is
  * pressed.
- * 
+ *
  * Since the server sends back a prettified version of the message with the
  * user's username and timestamp, the user's original message can be deleted
  * from the user's terminal so it can be replaced with the server's response.
  *
  * @param buffer The stdin buffer which represents the user's message.
  */
-void read_buffer(char* buffer);
+void read_buffer(char *buffer);
 
 /**
  * Writes a packet to the server's and client's shared socket.
- * 
+ *
  * The packet is generated on the user's side as a string containing
  * the user's username, timestamp of the message, and the message itself.
  * This is sent to the server using the write_to_server function.
  *
  * @param sockfd An integer representing the socket the client is connected to.
- * @param packet A string containing the username, timestamp of message, and 
+ * @param packet A string containing the username, timestamp of message, and
  *               message.
- * @param read_fd An fd_set struct used to clear the file descriptor from the socket. *************** What
+ * @param read_fd An fd_set struct used to clear the file descriptor from the
+ * socket. *************** What
  */
-void write_to_server(int sockfd, char* packet, fd_set read_fd);
+void write_to_server(int sockfd, char *packet, fd_set read_fd);
 
 /**
  * Reads incoming data from the server and prints it to the user's terminal.
- * 
- * The packet received contains the status of if the retreival failed or if the
- * connection to the server was lost. If none of these are true, the received packet
- * is used to add a null character to the end of the received string buffer. The 
- * buffer contains a string representing the user's username, timestamp of the
- * message, and message. This buffer is then printed to the user's terminal.
  *
- * @param packet_received An integer representing the status of the received packet
- *                        or the length of the buffer.
+ * The packet received contains the status of if the retreival failed or if the
+ * connection to the server was lost. If none of these are true, the received
+ * packet is used to add a null character to the end of the received string
+ * buffer. The buffer contains a string representing the user's username,
+ * timestamp of the message, and message. This buffer is then printed to the
+ * user's terminal.
+ *
+ * @param packet_received An integer representing the status of the received
+ * packet or the length of the buffer.
  * @param buffer The buffer that contains the packet data from the server.
  */
-int read_from_server(int packet_received, char* buffer);
+int read_from_server(int packet_received, char *buffer);
 
 #endif
